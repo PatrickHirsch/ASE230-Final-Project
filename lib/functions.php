ab2c8f9f2a6351e99a35a1ab2ddcaa3d889f5198
Sarah Riley <sarahriley@Sarahs-MacBook-Pro-2.local>
2023-10-10 12:52:35 -0400

    added a function to convert dateJoined to our current time and date. Arranged the cards correctly on admin.php

--- lib/functions.php
+++ lib/functions.php
@@ -1,5 +1,16 @@
 <?php
 //Generates user cards for admin.php
+
+function convertTimeStamp($user){
+    date_default_timezone_set('America/Kentucky/Louisville');
+    $userTimeStamp = $user['dateJoined'];
+    $formattedDateTime = date('Y-m-d H:i:s', $userTimeStamp);
+    return $formattedDateTime;
+}
+
+function checkAdmin($userData){
+
+}
 function generateAdminUserCards($userData)
 {
 
@@ -22,22 +33,24 @@ function generateAdminUserCards($userData)
         echo '<div class="col mb-5">
                         <div class="card h-100">
                             <!-- User Profile image-->
-                            <img class="card-img-top" src="'.$user['userProfileImage'].'" alt="Image of '.$user['name'].'" />
+                            <img class="card-img-top" src="' . $user['userProfileImage'] . '" alt="Image of ' . $user['name'] . '" />
                             <!-- User details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- User name-->
                                     <h5 class="fw-bolder">' . $user['name'] . '</h5>
                                     <!-- User Start Date-->
-                                    <div class="text-center">' . $user['dateJoined'] . '</div>
+                                    <div class="text-center">' . convertTimeStamp($user) . '</div>
                                     <!-- User Status-->
                                     <div class="text-center">' . $userStatus . '</div>
                                 </div>
                             </div>
                             <!-- User actions-->
+                            <form method="POST" action="">
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="user.php?ID=' . $user['ID'] . '">Check out User</a></div>
                             </div>
+                            </form>
                         </div>
                     </div>';
     }
@@ -45,32 +58,35 @@ function generateAdminUserCards($userData)
 }
 
 //generates user cards for index.php
-Function generateUserCards($userData){
+function generateUserCards($userData)
+{
 
-        foreach ($userData as $user) {
-            $status = $user['status'];
-           if ($status == 1 || $status == 3){
+    foreach ($userData as $user) {
+        $status = $user['status'];
+        if ($status == 1 || $status == 3) {
             echo '<div class="col mb-5">
                             <div class="card h-100">
                                 <!-- User Profile image-->
-                                <img class="card-img-top" src="'.$user['userProfileImage'].'" alt="Image of '.$user['name'].'" />
+                                <img class="card-img-top" src="' . $user['userProfileImage'] . '" alt="Image of ' . $user['name'] . '" />
                                 <!-- User details-->
                                 <div class="card-body p-4">
                                     <div class="text-center">
                                         <!-- User name-->
                                         <h5 class="fw-bolder">' . $user['name'] . '</h5>
                                         <!-- User Start Date-->
-                                        <div class="text-center">' . $user['dateJoined'] . '</div>
+                                        <div class="text-center">' . convertTimeStamp($user) . '</div>
                                     </div>
                                 </div>
                                 <!-- User actions-->
+                                <form method="POST" action="">
                                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="user.php?ID=' . $user['ID'] . '">Check out User</a></div>
                                 </div>
+                                </form>
                             </div>
                         </div>';
         }
-    
+
     }
 }
 
