078d4d8e6d62975d05ca4a352b60c029df75b8ff
Sarah Riley <sarahriley@Sarahs-MacBook-Pro-2.local>
2023-10-10 12:23:50 -0400

    Added functions for generateUserCards for the index.php page and generateAdminUserCards for admin.php

--- lib/functions.php
+++ lib/functions.php
@@ -1,19 +1,37 @@
 <?php
 //Generates user cards for index and admin page
-function generateUserCards($userData){
-    echo '<div class="col mb-5">
+function generateAdminUserCards($userData)
+{
+
+    foreach ($userData as $user) {
+        $status = $user['status'];
+        switch ($status) {
+            case (-1):
+                $userStatus = "Admin Blocked";
+                break;
+            case (0):
+                $userStatus = "User Deleted";
+                break;
+            case (1):
+                $userStatus = "Active User";
+                break;
+            case (3):
+                $userStatus = "Admin";
+                break;
+        }
+        echo '<div class="col mb-5">
                         <div class="card h-100">
                             <!-- User Profile image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="'.$user['userProfileImage'].'" alt="Image of '.$user['name'].'" />
                             <!-- User details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- User name-->
-                                    <h5 class="fw-bolder">Sale Item</h5>
+                                    <h5 class="fw-bolder">' . $user['name'] . '</h5>
                                     <!-- User Start Date-->
-                                    <div class="text-center">'.$userData['dateJoined'].'</div>
+                                    <div class="text-center">' . $user['dateJoined'] . '</div>
                                     <!-- User Status-->
-                                    <div class="text-center">'.$userData['status'].'
+                                    <div class="text-center">' . $userStatus . '</div>
                                 </div>
                             </div>
                             <!-- User actions-->
@@ -22,7 +40,38 @@ function generateUserCards($userData){
                             </div>
                         </div>
                     </div>';
+    }
 
 }
+
+Function generateUserCards($userData){
+
+        foreach ($userData as $user) {
+            $status = $user['status'];
+           if ($status == 1 || $status == 3){
+            echo '<div class="col mb-5">
+                            <div class="card h-100">
+                                <!-- User Profile image-->
+                                <img class="card-img-top" src="'.$user['userProfileImage'].'" alt="Image of '.$user['name'].'" />
+                                <!-- User details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- User name-->
+                                        <h5 class="fw-bolder">' . $user['name'] . '</h5>
+                                        <!-- User Start Date-->
+                                        <div class="text-center">' . $user['dateJoined'] . '</div>
+                                    </div>
+                                </div>
+                                <!-- User actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                </div>
+                            </div>
+                        </div>';
+        }
+    
+    }
+}
+
 
 ?>
