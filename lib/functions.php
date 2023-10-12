9d1ee0d669ad001132dc33e2a7705299eb6fa5c1
lukenorberg <p.lukenorberg@proton.me>
2023-10-12 18:39:15 -0400

    Added photo uploading/editing/deleting with HirschP2 Branch

---
+++ lib/functions.php
@@ -0,0 +1,284 @@
+<?php
+function importJSON($filePath)
+{	$contents=file_get_contents($filePath);
+    if(strpos($contents,"<?php")===0)
+    {	$contents=explode("\n",$contents);
+        array_shift($contents);
+        $contents=implode("\n",$contents);
+    }
+    return json_decode($contents,true);
+}
+
+function writeJSON($newArray,$filePath)
+{   $contents= json_encode($newArray, JSON_PRETTY_PRINT);
+    file_put_contents($filePath,$contents);
+}
+
+/**
+ * Finds the index of a photo
+ *
+ * @param array $imagesJson an associative array of image data
+ * @param string|int $photoId id of the photo to find
+ *
+ * @return string|int|null returns a string or int number, or null if not found
+ */
+function findJsonIdIndex($imagesJson, $photoId) {
+    $selectedImageIndex = null;
+
+// sets selected image to the image in the GET req
+    foreach ($imagesJson as $index => $image) {
+        if ($image['id'] === $photoId) {
+            $selectedImageIndex = $index;
+            break;
+        }
+    }
+    return $selectedImageIndex;
+}
+
+function generateAlbum($rootPath='.')
+{
+    $imagesArray=importJSON($rootPath.'/data/images.json');
+
+    if (!isset($imagesArray)) {
+        return '<div>No images at this time</div>';
+    }
+
+
+    $ret='
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    ';
+    foreach($imagesArray as $img) $ret=$ret.generateAlbumSquare($img,$rootPath);
+    $ret=$ret.'
+                </div>
+            </div>
+        </section>
+	';
+    return $ret;
+}
+
+function generateUserAlbum($userID,$rootPath='.')
+{	$imagesArray=importJSON($rootPath.'/data/images.json');
+
+    $ret='
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    ';
+    foreach($imagesArray as $img)
+        if($img['owner']==$userID)
+            $ret=$ret.generateAlbumSquare($img,$rootPath);
+
+    $ret=$ret.'
+                </div>
+            </div>
+        </section>
+	';
+    return $ret;
+}
+
+function generateAlbumSquare($img,$rootPath='.', $viewImageButton=true)
+{
+    $ret='
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <!-- div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div -->
+                            <!-- Product image-->
+                            <img class="card-img-top" src="'. $img['url'] .'" alt="' . $img['name'] . ' image" />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">'.$img['name'].'</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">';
+    for($star=1;$star<=$img['rating'];$star++)	$ret=$ret.'<div class="bi-star-fill"></div>';
+    $ret=$ret.'</div>
+                                    <!-- Product price-->
+                                    <a class="text-muted" href="user.php?id='.$img['owner'].'">'.getUserName($img['owner']).'</a>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            ';
+                            if ($viewImageButton) $ret=$ret.'
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="' . './' . 'image.php?photoid=' . $img['id'] . '">View image</a></div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="' . './' . 'editImage.php?photoid=' . $img['id'] . '">Edit/Delete image</a></div>
+                            </div>
+                        </div>
+                    </div>
+	';
+    return $ret;
+}
+
+function getUserObject($lookup,$field='ID')
+{
+    $allUsers=importJSON('data/users.json');
+    foreach($allUsers as $user) {
+        if($user[$field]==$lookup) return $user;
+    }
+    return null;
+}
+
+function getUserName($userID)
+{
+    return getUserObject($userID)['name'];
+}
+
+function getImageObject($lookup,$field='ID')
+{
+    $allImages=importJSON('data/images.json');
+    foreach($allImages as $img)
+        if($img[$field]==$lookup)
+            return $img;
+    return null;
+}
+
+function getImageObjects($lookup,$field='owner')
+{	$allImages=importJSON('data/images.json');
+    $ret=[];
+    foreach($allImages as $img)
+        if($img[$field]==$lookup)
+            $ret[]=$img;
+    return $ret;
+}
+
+function getUsersPhotos($userID)
+{	$allImages=importJSON('data/images.json');
+    //foreach image, if ownerID==userID add to $ret
+    return $allImages;
+}
+
+function generateUserCard()
+{
+    $userData=importJSON('data/users.json');
+
+    foreach ($userData as $user) {
+        $status = $user['status'];
+        if ($status == 1 || $status == 3) {
+            echo '<div class="col mb-5">
+                            <div class="card h-100">
+                                <!-- User Profile image-->
+                                <img class="card-img-top" src="' . $user['userProfileImage'] . '" alt="Image of ' . $user['name'] . '" />
+                                <!-- User details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- User name-->
+                                        <h5 class="fw-bolder">' . $user['name'] . '</h5>
+                                        <!-- User Start Date-->
+                                        <div class="text-center">' . convertTimeStamp($user) . '</div>
+                                    </div>
+                                </div>
+                                <!-- User actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="user.php?ID=' . $user['ID'] . '">Check out User</a></div>
+                                </div>
+                            </div>
+                        </div>';
+        }
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+//Generates user cards for admin.php
+
+function convertTimeStamp($user)
+{
+    date_default_timezone_set('America/Kentucky/Louisville');
+    $userTimeStamp = $user['dateJoined'];
+    $formattedDateTime = date('Y-m-d H:i:s', $userTimeStamp);
+    return $formattedDateTime;
+}
+
+//function checkStatus($userData){
+//   if (isset($_SESSION['user_id'])) {
+
+
+//    }
+//}
+function generateAdminUserCards($userData)
+{
+
+    foreach ($userData as $user) {
+        $status = $user['status'];
+        switch ($status) {
+            case (-1):
+                $userStatus = "Admin Blocked";
+                break;
+            case (0):
+                $userStatus = "User Deleted";
+                break;
+            case (1):
+                $userStatus = "Active User";
+                break;
+            case (3):
+                $userStatus = "Admin";
+                break;
+        }
+        echo '<div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- User Profile image-->
+                            <img class="card-img-top" src="' . $user['userProfileImage'] . '" alt="Image of ' . $user['name'] . '" />
+                            <!-- User details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- User name-->
+                                    <h5 class="fw-bolder">' . $user['name'] . '</h5>
+                                    <!-- User Start Date-->
+                                    <div class="text-center">' . convertTimeStamp($user) . '</div>
+                                    <!-- User Status-->
+                                    <div class="text-center">' . $userStatus . '</div>
+                                </div>
+                            </div>
+                            <!-- User actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="user.php?ID=' . $user['ID'] . '">Check out User</a></div>
+                            </div>
+                            </form>
+                        </div>
+                    </div>';
+    }
+
+}
+
+//generates user cards for index.php
+function generateUserCards($userData)
+{
+
+    foreach ($userData as $user) {
+        $status = $user['status'];
+        if ($status == 1 || $status == 3) {
+            echo '<div class="col mb-5">
+                            <div class="card h-100">
+                                <!-- User Profile image-->
+                                <img class="card-img-top" src="' . $user['userProfileImage'] . '" alt="Image of ' . $user['name'] . '" />
+                                <!-- User details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- User name-->
+                                        <h5 class="fw-bolder">' . $user['name'] . '</h5>
+                                        <!-- User Start Date-->
+                                        <div class="text-center">' . convertTimeStamp($user) . '</div>
+                                    </div>
+                                </div>
+                                <!-- User actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="user.php?ID=' . $user['ID'] . '">Check out User</a></div>
+                                </div>
+                            </div>
+                        </div>';
+        }
+
+    }
+}
+
+
+?>
---
+++ login.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+require_once 'header.php';
+require_once 'lib/functions.php';
+
+// Check if the success parameter is present in the URL
+if (isset($_SESSION['success_message'])) {
+    echo '<p style="color: green;">' . $_SESSION['success_message'] . '</p>';
+    unset($_SESSION['success_message']); // Clear the session variable
+}
+// Check if the user is already logged in and redirect them to user.php
+if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
+    header('Location: user.php');
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        //var_dump($email);
+        //var_dump($password);
+        // Read the user data from the JSON file
+        $userData = json_decode(file_get_contents('data/users.json'), true);
+
+        // Loop through user data to find a matching user
+        foreach ($userData as $user) {
+            if ($user['email'] === $email && password_verify($password, $user['password'])) {
+                // Store user data in the session
+                $_SESSION['email'] = $email;
+                $_SESSION['user_name'] = $user['name'];
+                $_SESSION['user_id']=$user['ID'];
+                $_SESSION['user_status']=$user['status'];
+
+                if($_SESSION['user_status'] != 1 || $_SESSION['user_status'] != 3 ){
+                    if($_SESSION['user_status'] == 0){
+                        echo "Your account has been deleted. Would you like to restore your account?<br>
+                        <form method=\"POST\" action=\"\">
+                        <input type=\"hidden\" name=\"email\" value=\"$email\">
+                            <input type=\"hidden\" name=\"password\" value=\"$password\">
+                            <button name=\"restore\" type=\"submit\">Restore Account</button></form>";
+                        die();
+
+                    }
+                    if($_SESSION['user_status'] == -1){
+                        die("Your account has been removed by admin.");
+                    }
+                }
+
+                // Redirect to user.php
+                header('Location: user.php');
+                exit();
+            }
+                if (isset($_POST['restore'])) {
+                    $restoredEmail = $_POST['email'];
+                    $restoredPassword = $_POST['password'];
+
+                    foreach ($userData as &$user) {
+                        if ($user['email'] === $restoredEmail && password_verify($restoredPassword, $user['password'])) {
+                            $user['status']= "1";
+                            var_dump($user);
+                            file_put_contents('data/users.json', json_encode($userData, JSON_PRETTY_PRINT));
+                            var_dump($userData);
+                            $_SESSION['success_message'] = "Your account has been restored. Please login.";
+                        }
+                    }
+                    header('Location: login.php');
+                }
+        }
+
+        // If no matching user is found, display an error message
+        echo 'Invalid email or password. Please try again or Create an account';
+    }
+}
+
+?>
+<?= echoHeader('User Login') ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <section style="background-color: #eee;">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-lg-12 col-xl-11">
+                    <div class="card text-black" style="border-radius: 25px;">
+                        <div class="card-body p-md-5">
+                            <div class="row justify-content-center">
+                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                                        <!-- Email input -->
+                                        <div class="form-outline mb-4">
+                                            <input type="email" id="form2Example1" name='email' class="form-control" />
+                                            <label class="form-label" for="form2Example1">Email address</label>
+                                        </div>
+
+                                        <!-- Password input -->
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="form2Example2" name='password'
+                                                class="form-control" />
+                                            <label class="form-label" for="form2Example2">Password</label>
+                                        </div>
+
+                                        <!-- 2 column grid layout for inline styling -->
+                                        <div class="row mb-4">
+                                            <div class="col d-flex justify-content-center">
+                                                <!-- Checkbox 
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value=""
+                                                        id="form2Example31" checked />
+                                                    <label class="form-check-label" for="form2Example31"> Remember me
+                                                    </label>
+                                                </div>
+                                            </div>
+
+                                            <div class="col">-->
+                                                <!-- Simple link -->
+                                                <!-- a href="#!">Forgot password?</a> -->
+                                            </div>
+                                        </div>
+
+                                        <!-- Submit button -->
+                                        <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                                        <!-- Register buttons -->
+                                        <div class="text-center">
+                                            <p>Not a member? <a href="signup.php">Register</a></p>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+<?= echoFooter(); ?>
