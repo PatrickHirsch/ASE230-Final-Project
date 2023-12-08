65262c9ea5647e3d1073c6312db9262fc2a6a6c7
Saraphoo <smriley97@gmail.com>
2023-12-08 01:27:26 -0500

    Database should updatedate correctly. Button has been added for users to remove their account. Full functionality not yet implimented.

--- db/db.php
+++ db/db.php
@@ -1,9 +1,9 @@
-<?php $host = '127.0.0.1';
+<?php $host = '127.0.0.1'; //calibrated for my local port
 $db = 'MyPhotoVault';
-$user = 'root';
-$pass = 'example';
+$user = 'root';//calibrated for my local port
+$pass = 'example';//calibrated for my local port
 $charset = 'utf8';
-$dsn = "mysql:host=$host; port=3306 ;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host; port=3306 ;dbname=$db;charset=$charset";//calibrated for my local port
 $opt = [
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
